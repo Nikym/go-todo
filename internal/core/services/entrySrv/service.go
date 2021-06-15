12047584807503715b8d0783a0b09af035bb4f2e
Nikita Melnikov <nik.y.melnikov@gmail.com>
2021-06-15 21:58:24 +0100

    Refined Get entry service method and added Create interface method

--- internal/core/services/entrySrv/service.go
+++ internal/core/services/entrySrv/service.go
@@ -1,15 +1,42 @@
 package entrySrv
 
-import "github.com/Nikym/go-todo/internal/core/domain"
+import (
+	"errors"
+	"github.com/Nikym/go-todo/internal/core/domain"
+	"github.com/Nikym/go-todo/internal/core/ports"
+)
 
-type service struct{}
+type service struct {
+	entryRepository ports.EntryRepository
+}
 
 // New returns a pointer to a new service object.
-func New() *service {
-	return &service{}
+func New(repository ports.EntryRepository) *service {
+	return &service{
+		entryRepository: repository,
+	}
 }
 
 // Get returns the domain.Entry object with the given UUID.
 func (srv *service) Get(id string) (domain.Entry, error) {
-	return domain.Entry{}, nil
+	entry, err := srv.entryRepository.Get(id)
+	if err != nil {
+		return domain.Entry{}, errors.New("retrieving entry from repository failed")
+	}
+
+	return entry, nil
+}
+
+// Create makes a new domain.Entry object and saves it to the repository.
+func (srv *service) Create(title, description string) (domain.Entry, error) {
+	if len(title) < 3 {
+		return domain.Entry{}, errors.New("title must consist of 3 characters or more")
+	}
+
+	entry := domain.NewEntry(title, description)
+	if err := srv.entryRepository.Save(*entry); err != nil {
+		return domain.Entry{}, errors.New("saving entry to repository failed")
+	}
+
+	return *entry, nil
 }
