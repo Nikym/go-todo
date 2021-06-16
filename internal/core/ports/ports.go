377921f4c36bcec6356f76d73f9318731a2475b6
Nikita Melnikov <nik.y.melnikov@gmail.com>
2021-06-16 11:02:37 +0100

    Refined port interfaces to use pointers for domain.Entry objects

--- internal/core/ports/ports.go
+++ internal/core/ports/ports.go
@@ -5,16 +5,16 @@ import "github.com/Nikym/go-todo/internal/core/domain"
 // EntryRepository is the interface for the repository port handling the
 // retrieval and storage of to-do entries.
 type EntryRepository interface {
-	Get(id string) (domain.Entry, error)
-	Save(entry domain.Entry) error
+	Get(id string) (*domain.Entry, error)
+	Save(entry *domain.Entry) error
 	Delete(id string) error
 }
 
 // EntryService is the interface for the driver port handling the
 // interactions with entries (domain.Entry)
 type EntryService interface {
-	Get(id string) (domain.Entry, error)
-	Create(title, description string) (domain.Entry, error)
-	Update(id string, entry domain.Entry) (domain.Entry, error)
+	Get(id string) (*domain.Entry, error)
+	Create(title, description string) (*domain.Entry, error)
+	Update(id string, entry *domain.Entry) error
 	Delete(id string) error
 }
--- internal/core/services/entrySrv/service.go
+++ internal/core/services/entrySrv/service.go
@@ -18,25 +18,25 @@ func New(repository ports.EntryRepository) *service {
 }
 
 // Get returns the domain.Entry object with the given UUID.
-func (srv *service) Get(id string) (domain.Entry, error) {
+func (srv *service) Get(id string) (*domain.Entry, error) {
 	entry, err := srv.entryRepository.Get(id)
 	if err != nil {
-		return domain.Entry{}, errors.New("retrieving entry from repository failed")
+		return &domain.Entry{}, errors.New("retrieving entry from repository failed")
 	}
 
 	return entry, nil
 }
 
 // Create makes a new domain.Entry object and saves it to the repository.
-func (srv *service) Create(title, description string) (domain.Entry, error) {
+func (srv *service) Create(title, description string) (*domain.Entry, error) {
 	if len(title) < 3 {
-		return domain.Entry{}, errors.New("title must consist of 3 characters or more")
+		return &domain.Entry{}, errors.New("title must consist of 3 characters or more")
 	}
 
 	entry := domain.NewEntry(title, description)
-	if err := srv.entryRepository.Save(*entry); err != nil {
-		return domain.Entry{}, errors.New("saving entry to repository failed")
+	if err := srv.entryRepository.Save(entry); err != nil {
+		return &domain.Entry{}, errors.New("saving entry to repository failed")
 	}
 
-	return *entry, nil
+	return entry, nil
 }
